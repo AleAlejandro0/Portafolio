3edbfd8470d162eb0fa8f6b82993a01eb898ed62
AleAlejandro0 <alealejandropaez@gmail.com>
2022-09-28 13:42:03 -0500

    Peronal information was added, bugs were fixed, navbar fuctionality

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+let navMenu = document.querySelector(".nav_img")
+let menuOverlay = document.querySelector(".nav_menu")
+
+
+ /* CLOSE AN DOPEN MENU */
+navMenu.addEventListener("click", () => {
+  menuOverlay.classList.toggle("show")
+})
+
+let navOptions = document.querySelectorAll(".nav_item")
+
+for(let option of navOptions){
+  option.addEventListener("click", () => {
+  menuOverlay.classList.toggle("show")})
+}
+
+//JUMP TO PORTFOLIO
+
+let btn = document.getElementsByClassName("btn__portfolio")
+
+console.log(btn)
+btn[0].addEventListener("click", () => {
+  window.location.hash = "nav-portfolio";
+})
