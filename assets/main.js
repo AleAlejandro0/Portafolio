ff09aceed50eee7d8312c2995c4bd9de470cb771
AleAlejandro0 <alealejandropaez@gmail.com>
2022-10-16 21:17:22 -0500

    Spanish Lenguage added

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,20 @@ for(let option of navOptions){
   option.addEventListener("click", () => {
   menuOverlay.classList.toggle("show")})
 }
+
+
+// CHAGE TO SPANISH OR ENGLISH LENGUAGE 
+
+let check = document.querySelector('.check')
+
+check.addEventListener('click', () => {
+  let id = check.checked
+
+  if(id == true){
+    location.href =  '/Spanish/index.html'
+  }else{
+    location.href = '/index.html'
+  }
+
+})
+
